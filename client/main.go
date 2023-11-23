27ce707e74b294c7c6d9f55d80cc81cef021b997
Abu Zakaria <asm.zakaria120@gmail.com>
2023-11-24 01:47:02 +0600

    Update

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"json"
 	"log"
 	"net"
 	"os"
@@ -18,15 +19,15 @@ func main() {
 
 	server_address := args[1]
 
-	conn, err := net.Dial("tcp", server_address)
-	if err != nil {
-		log.Println("Couldn't connect with the server")
-		log.Println("ERROR:", err)
-	}
-
-	defer conn.Close()
-
 	for {
+		conn, err := net.Dial("tcp", server_address)
+		if err != nil {
+			log.Println("Couldn't connect with the server")
+			log.Println("ERROR:", err)
+		}
+
+		defer conn.Close()
+
 		fmt.Print("> ")
 
 		reader := bufio.NewReader(os.Stdin)
@@ -39,9 +40,17 @@ func main() {
 		conn.Write([]byte(text))
 		log.Println("sent message!")
 
-		incoming_msg := make([]byte, 64)
+		incoming_msg := make([]byte, 1064)
 		conn.Read(incoming_msg)
 
-		fmt.Println("incoming message:", incoming_msg)
+		var messages []string
+
+		err = json.Unmarshal(incoming_msg, &messages)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		fmt.Println("incoming message:", messages)
 	}
 }
--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"json"
 	"log"
 	"net"
 	"slices"
@@ -10,6 +11,7 @@ import (
 
 func main() {
 	registered_users := []string{}
+	messages := []string{}
 
 	listener, err := net.Listen("tcp", "0.0.0.0:8080")
 	if err != nil {
@@ -42,16 +44,19 @@ func main() {
 		log.Println("incoming message length: ", length)
 		log.Println("incoming message: ", string(msg))
 
+		messages = append(messages, remote_addr.String()+" => "+string(msg))
+
 		fmt.Println("registered:", registered_users)
 
 		for _, address := range registered_users {
-			sendMessage(conn, address, msg)
+			sendMessages(conn, address, messages)
 		}
 	}
 }
 
-func sendMessage(conn net.Conn, address string, message []byte) error {
-	length, err := conn.Write(message)
+func sendMessages(conn net.Conn, address string, messages []string) error {
+	json_string := json.Marshal(messages)
+	length, err := conn.Write([]byte(json_string))
 	if err != nil {
 		return errors.New("sendMessage(): Something went sending message to:" + address)
 	}
